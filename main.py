541dde5fd7e53f54dbfd533191842f3ca77b7163
machenjie <309272985@qq.com>
2022-09-13 15:45:56 +0800

    update

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def scale_to_size(writer,page, unit_size):
 
 
 writer = PdfFileWriter()
-target_path = './source'
-pdf_lst = [f for f in os.listdir(target_path) if f.endswith('.pdf')]
-pdf_lst = [os.path.join(target_path, filename) for filename in pdf_lst]
+source_path = './source'
+pdf_lst = [f for f in os.listdir(source_path) if f.endswith('.pdf')]
+pdf_lst = [os.path.join(source_path, filename) for filename in pdf_lst]
 
 file_merger = PdfMerger()
 for pdf in pdf_lst:
